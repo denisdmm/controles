28239301d2f9e4b317cd90b907d97172882a662c
Denis Moura <denisdmm@fab.mil.br>
2023-09-27 10:43:14 -0300

    criar menu topo

--- src/app/main/webapp/layouts/navbar/navbar.component.ts
+++ src/app/main/webapp/layouts/navbar/navbar.component.ts
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  userImage!: string;
+  constructor() {
+    this.userImage = '/assets/imagens/userCircle.png';
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
   }
